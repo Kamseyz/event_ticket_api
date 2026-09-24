8bf985c0c2f42ca5a6a671f291299df042c24260
Dike Ebuka Christopher <ebukadike001@gmail.com>
2026-09-24 07:29:44 +0100

    fix:changed message to default_detail

--- core/booking/execption.py
+++ core/booking/execption.py
@@ -2,19 +2,19 @@ from rest_framework.exceptions import APIException
 from rest_framework import status
 
 
-
+#insufficent capactiy error
 class InsufficientCapacityError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
-    message = "Insufficient Capacity"
+    default_detail = "Insufficient Capacity"
     
     
-    
+#events/tier not found error    
 class TierNotFoundError(APIException):
     status_code=status.HTTP_404_NOT_FOUND
-    message="Ticket Tier Doesn't exist"
+    default_detail="Ticket Tier Doesn't exist"
     
     
-    
+#invalid quantity error    
 class InvalidQuantityError(APIException):
     status_code =status.HTTP_400_BAD_REQUEST
-    message="Quantity can't be zero or less than zero"
+    default_detail="Quantity can't be zero or less than zero"
