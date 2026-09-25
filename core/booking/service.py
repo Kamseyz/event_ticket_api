2802c1515477cba15f65fb4564cca0e8c50c1e3a
Dike Ebuka Christopher <ebukadike001@gmail.com>
2026-09-25 09:33:42 +0100

    fix: i added transaction.atomic + select_for_update to prevent booking oversell

--- core/booking/service.py
+++ core/booking/service.py
@@ -5,7 +5,7 @@ from .execption import(
     InvalidQuantityError
 )
 from events.models import TicketTier
-from django.contrib.auth import get_user_model
+from django.db import transaction
 
 
 
@@ -16,34 +16,34 @@ def create_booking(user, tier_id: int, quantity: int)-> dict:
     if quantity <= 0:
         raise InvalidQuantityError("Quantity cannot be zero or less than zero")
     
+     #check if the event exist
+    with transaction.atomic():
+        try:
+            tier= TicketTier.objects.select_for_update().get(id=tier_id)
+        except TicketTier.DoesNotExist:
+            raise TierNotFoundError(f"Event with this id {tier_id} doesn't exist")
+        
+        
+         #check for insufficient capacity
+        remainder= tier.capacity - tier.tickets_sold
+        
+        if quantity > remainder:
+            raise InsufficientCapacityError(f"Insufficient capacity only {tier.capacity} is avaiable")
     
-    #check if the event exist
-    try:
-        tier = TicketTier.objects.get(id=tier_id)
-    except TicketTier.DoesNotExist:
-        raise TierNotFoundError(f"Event with this id {tier_id} doesn't exist")
     
-    
-    #check for insufficient capacity
-    remainder= tier.capacity - tier.tickets_sold
-    if quantity > remainder:
-        raise InsufficientCapacityError(f"Insufficient capacity only {tier.capacity} is avaiable")
-    
-    
-    #create the booking and save 
-    book = Booking.objects.create(
-        user=user,
-        ticket_tier=tier,
-        quantity=quantity,
-        total_price=tier.price * quantity,
-        status="PENDING",
-    )
-
-    
-    #update the db 
-    tier.tickets_sold += quantity
-    tier.save()
-    
-    #return 
-    return book
+        #create the booking and save 
+        book = Booking.objects.create(
+            user=user,
+            ticket_tier=tier,
+            quantity=quantity,
+            total_price =tier.price * quantity,
+            status="PENDING"
+        )
+        
+        #update the db
+        tier.tickets_sold += quantity
+        tier.save()
+        
+        #return booking
+        return book
     
